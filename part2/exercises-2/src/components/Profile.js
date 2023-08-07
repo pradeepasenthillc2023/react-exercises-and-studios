71baee27d8002ad386849995cdbd8326a1b37a34
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-08-07 16:51:00 -0400

    status

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,12 +1,28 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
+const listItem = oceans.map(ocean =>
+   <div key={ocean.id} className="profile" >
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+   </div>
+
+);
 
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+      <h1>{oceans.name}</h1>
+      <h3>Fun Facts:</h3>
+   <ul>
+      <li>{oceans.fact1}</li>
+      <li>{oceans.fact2}</li>
+      <li>{oceans.fact3}</li>
+   </ul>
+         <ul>
+            {listItem}
+         </ul>  
          <Button />
       </>  
    );
